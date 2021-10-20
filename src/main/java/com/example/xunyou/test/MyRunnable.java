ecc4e674fcb2f41e270b5123527c1b7c3b6f613a
ââwangzhen1024 <1039461479@qq.com>
2021-10-20 21:56:11 +0800

    第一次

--- src/main/java/com/example/xunyou/test/MyRunnable.java
+++
@@ -1,9 +0,0 @@
-package com.example.xunyou.test;
-
-public class MyRunnable implements Runnable{
-
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread()+"  run()方法执行");
-    }
-}
--- src/main/java/com/example/xunyou/test/MyThread.java
+++
@@ -1,9 +0,0 @@
-package com.example.xunyou.test;
-
-public class MyThread extends Thread {
-
-@Override
-	public void run() {
-        	System.out.println(Thread.currentThread().getName() + " run()方法正在执行...");
-        	}
-}
